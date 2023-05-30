016f4b6ba1b6e87510881538606db007f7e5e44f
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-05-31 02:47:06 +0900

    Reorganize models and tests

--- run-test.sh
+++ run-test.sh
@@ -2,56 +2,20 @@
 
 # vu-narrow
 ## half
-tmux new-session -d -s vu-narrow-half-states '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/half/vu-narrow-half-states.maude > ~/heuristic-narrowing/tests/vu-narrow/half/vu-narrow-half-states.out'
-tmux new-session -d -s vu-narrow-half-time '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/half/vu-narrow-half-time.maude > ~/heuristic-narrowing/tests/vu-narrow/half/vu-narrow-half-time.out'
 
-tmux new-session -d -s fvu-narrow-half-states '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/half/fvu-narrow-half-states.maude > ~/heuristic-narrowing/tests/vu-narrow/half/fvu-narrow-half-states.out'
-tmux new-session -d -s fvu-narrow-half-time '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/half/fvu-narrow-half-time.maude > ~/heuristic-narrowing/tests/vu-narrow/half/fvu-narrow-half-time.out'
+tmux new-session -d -s vu-narrow-onethirdrule-half '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/onethirdrule-half.maude > ~/heuristic-narrowing/tests/vu-narrow/onethirdrule-half.out'
+tmux new-session -d -s vu-narrow-onethirdrule-one '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/onethirdrule-one.maude > ~/heuristic-narrowing/tests/vu-narrow/onethirdrule-one.out'
 
-tmux new-session -d -s vu-narrow-half-three-objs-agreement '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/half/vu-narrow-half-three-objs.maude > ~/heuristic-narrowing/tests/vu-narrow/half/vu-narrow-half-three-objs.out'
-
-tmux new-session -d -s fvu-narrow-half-three-objs-agreement '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/half/fvu-narrow-half-three-objs.maude > ~/heuristic-narrowing/tests/vu-narrow/half/fvu-narrow-half-three-objs.out'
-## twothird
-tmux new-session -d -s vu-narrow-twothird-states '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/twothird/vu-narrow-twothird-states.maude > ~/heuristic-narrowing/tests/vu-narrow/twothird/vu-narrow-twothird-states.out'
-tmux new-session -d -s vu-narrow-twothird-time '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/twothird/vu-narrow-twothird-time.maude > ~/heuristic-narrowing/tests/vu-narrow/twothird/vu-narrow-twothird-time.out'
-
-tmux new-session -d -s fvu-narrow-twothird-states '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/twothird/fvu-narrow-twothird-states.maude > ~/heuristic-narrowing/tests/vu-narrow/twothird/fvu-narrow-twothird-states.out'
-tmux new-session -d -s fvu-narrow-twothird-time '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/twothird/fvu-narrow-twothird-time.maude > ~/heuristic-narrowing/tests/vu-narrow/twothird/fvu-narrow-twothird-time.out'
-
-tmux new-session -d -s vu-narrow-twothird-three-objs-states '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/twothird/vu-narrow-twothird-three-objs-states.maude > ~/heuristic-narrowing/tests/vu-narrow/twothird/vu-narrow-twothird-three-objs-states.out'
-tmux new-session -d -s vu-narrow-twothird-three-objs-time '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/twothird/vu-narrow-twothird-three-objs-time.maude > ~/heuristic-narrowing/tests/vu-narrow/twothird/vu-narrow-twothird-three-objs-time.out'
-
-tmux new-session -d -s fvu-narrow-twothird-three-objs-states '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/twothird/fvu-narrow-twothird-three-objs-states.maude > ~/heuristic-narrowing/tests/vu-narrow/twothird/fvu-narrow-twothird-three-objs-states.out'
-tmux new-session -d -s fvu-narrow-twothird-three-objs-time '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/twothird/fvu-narrow-twothird-three-objs-time.maude > ~/heuristic-narrowing/tests/vu-narrow/twothird/fvu-narrow-twothird-three-objs-time.out'
-
-# half
-tmux new-session -d -s onethirdrule-half '~/Maude/maude ~/heuristic-narrowing/tests/half/onethirdrule.maude > ~/heuristic-narrowing/tests/half/onethirdrule.out'
-tmux new-session -d -s onethirdrule-half-three-objs '~/Maude/maude ~/heuristic-narrowing/tests/half/onethirdrule-three-objs.maude > ~/heuristic-narrowing/tests/half/onethirdrule-three-objs.out'
-tmux new-session -d -s onethirdrule-guarded '~/Maude/maude ~/heuristic-narrowing/tests/half/onethirdrule-guarded.maude > ~/heuristic-narrowing/tests/half/onethirdrule-guarded.out'
-
-tmux new-session -d -s no-message-loss-half '~/Maude/maude ~/heuristic-narrowing/tests/half/no-message-loss.maude > ~/heuristic-narrowing/tests/half/no-message-loss.out'
-tmux new-session -d -s no-message-loss-half-three-objs '~/Maude/maude ~/heuristic-narrowing/tests/half/no-message-loss-three-objs.maude > ~/heuristic-narrowing/tests/half/no-message-loss-three-objs.out'
-tmux new-session -d -s no-message-loss-guarded '~/Maude/maude ~/heuristic-narrowing/tests/half/no-message-loss-guarded.maude > ~/heuristic-narrowing/tests/half/no-message-loss-guarded.out'
-
-# twothird
-tmux new-session -d -s onethirdrule-twothird-depth-bound '~/Maude/maude ~/heuristic-narrowing/tests/twothird/onethirdrule-twothird-depth-bound.maude > ~/heuristic-narrowing/tests/twothird/onethirdrule-twothird-depth-bound.out'
-
-tmux new-session -d -s no-message-loss-twothird-depth-bound '~/Maude/maude ~/heuristic-narrowing/tests/twothird/no-message-loss-twothird-depth-bound.maude > ~/heuristic-narrowing/tests/twothird/no-message-loss-twothird-depth-bound.out'
-
-tmux new-session -d -s onethirdrule-twothird-three-objs '~/Maude/maude ~/heuristic-narrowing/tests/twothird/onethirdrule-twothird-three-objs.maude > ~/heuristic-narrowing/tests/twothird/onethirdrule-twothird-three-objs.out'
-tmux new-session -d -s no-message-loss-twothird-three-objs '~/Maude/maude ~/heuristic-narrowing/tests/twothird/no-message-loss-twothird-three-objs.maude > ~/heuristic-narrowing/tests/twothird/no-message-loss-twothird-three-objs.out'
+tmux new-session -d -s vu-narrow-no-message-loss-half '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/no-message-loss-half.maude > ~/heuristic-narrowing/tests/vu-narrow/no-message-loss-half.out'
+tmux new-session -d -s vu-narrow-no-message-loss-one '~/Maude/maude ~/heuristic-narrowing/tests/vu-narrow/onethirdrule-one.maude > ~/heuristic-narrowing/tests/vu-narrow/onethirdrule-one.out'
 
 
-tmux new-session -d -s s1 '~/Maude/maude ~/transition-score/tests/temp/onethirdrule.maude > ~/transition-score/tests/temp/onethirdrule.out'
-tmux new-session -d -s s2 '~/Maude/maude ~/transition-score/tests/temp/onethirdrule-transition-score.maude > ~/transition-score/tests/temp/onethirdrule-transition-score.out'
-tmux new-session -d -s s3 '~/Maude/maude ~/transition-score/tests/temp/onethirdrule-static.maude > ~/transition-score/tests/temp/onethirdrule-static.out'
-tmux new-session -d -s s4 '~/Maude/maude ~/transition-score/tests/temp/onethirdrule-static-transition-score.maude > ~/transition-score/tests/temp/onethirdrule-static-transition-score.out'
-tmux new-session -d -s s5 '~/Maude/maude ~/transition-score/tests/temp/onethirdrule-static-dynamic.maude > ~/transition-score/tests/temp/onethirdrule-static-dynamic.out'
-tmux new-session -d -s s6 '~/Maude/maude ~/transition-score/tests/temp/onethirdrule-static-dynamic-transition-score.maude > ~/transition-score/tests/temp/onethirdrule-static-dynamic-transition-score.out'
+tmux new-session -d -s onethirdrule-half '~/Maude/maude ~/heuristic-narrowing/tests/heuristic-narrowing/onethirdrule-half.maude > ~/heuristic-narrowing/tests/heuristic-narrowing/onethirdrule-half.out'
+tmux new-session -d -s onethirdrule-one '~/Maude/maude ~/heuristic-narrowing/tests/heuristic-narrowing/onethirdrule-one.maude > ~/heuristic-narrowing/tests/heuristic-narrowing/onethirdrule-one.out'
+tmux new-session -d -s no-message-loss-half '~/Maude/maude ~/heuristic-narrowing/tests/heuristic-narrowing/no-message-loss-half.maude > ~/heuristic-narrowing/tests/heuristic-narrowing/no-message-loss-half.out'
+tmux new-session -d -s no-message-loss-one '~/Maude/maude ~/heuristic-narrowing/tests/heuristic-narrowing/no-message-loss-one.maude > ~/heuristic-narrowing/tests/heuristic-narrowing/no-message-loss-one.out'
 
-tmux new-session -d -s s7 '~/Maude/maude ~/transition-score/tests/temp/no-message-loss.maude > ~/transition-score/tests/temp/no-message-loss.out'
-tmux new-session -d -s s8 '~/Maude/maude ~/transition-score/tests/temp/no-message-loss-transition-score.maude > ~/transition-score/tests/temp/no-message-loss-transition-score.out'
-tmux new-session -d -s s9 '~/Maude/maude ~/transition-score/tests/temp/no-message-loss-static.maude > ~/transition-score/tests/temp/no-message-loss-static.out'
-tmux new-session -d -s s10 '~/Maude/maude ~/transition-score/tests/temp/no-message-loss-static-transition-score.maude > ~/transition-score/tests/temp/no-message-loss-static-transition-score.out'
-tmux new-session -d -s s11 '~/Maude/maude ~/transition-score/tests/temp/no-message-loss-static-dynamic.maude > ~/transition-score/tests/temp/no-message-loss-static-dynamic.out'
-tmux new-session -d -s s12 '~/Maude/maude ~/transition-score/tests/temp/no-message-loss-static-dynamic-transition-score.maude > ~/transition-score/tests/temp/no-message-loss-static-dynamic-transition-score.out'
+tmux new-session -d -s onethirdrule-half-prime '~/Maude/maude ~/heuristic-narrowing/tests/heuristic-narrowing/onethirdrule-half-prime.maude > ~/heuristic-narrowing/tests/heuristic-narrowing/onethirdrule-half-prime.out'
+tmux new-session -d -s onethirdrule-one-prime '~/Maude/maude ~/heuristic-narrowing/tests/heuristic-narrowing/onethirdrule-one-prime.maude > ~/heuristic-narrowing/tests/heuristic-narrowing/onethirdrule-one-prime.out'
+tmux new-session -d -s no-message-loss-half-prime '~/Maude/maude ~/heuristic-narrowing/tests/heuristic-narrowing/no-message-loss-half-prime.maude > ~/heuristic-narrowing/tests/heuristic-narrowing/no-message-loss-half-prime.out'
+tmux new-session -d -s no-message-loss-one-prime '~/Maude/maude ~/heuristic-narrowing/tests/heuristic-narrowing/no-message-loss-one-prime.maude > ~/heuristic-narrowing/tests/heuristic-narrowing/no-message-loss-one-prime.out'
