55d77c23e31300646954ed7349065b01307b070f
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-11-28 22:09:44 -0500

    Fix tests

--- run-test-default-indexing.sh
+++ run-test-default-indexing.sh
@@ -2,27 +2,27 @@
 
 # heuristic-narrow
 tmux new-session -d -s heuristic-arbitrary-half-default-indexing \
-'~/Maude/maude ~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-half.maude > \
-~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-half.maude > \
+~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-half.out'
 tmux new-session -d -s heuristic-arbitrary-one-default-indexing \
-'~/Maude/maude ~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-one.maude > \
-~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-one.maude > \
+~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-one.out'
 tmux new-session -d -s heuristic-arbitrary-nml-half-default-indexing \
-'~/Maude/maude ~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-nml-half.maude > \
-~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-nml-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-nml-half.maude > \
+~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-nml-half.out'
 tmux new-session -d -s heuristic-arbitrary-nml-one-default-indexing \
-'~/Maude/maude ~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-nml-one.maude > \
-~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-nml-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-nml-one.maude > \
+~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/arbitrary/onethirdrule-nml-one.out'
 
 tmux new-session -d -s heuristic-three-objs-half-default-indexing \
-'~/Maude/maude ~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-half.maude > \
-~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-half.maude > \
+~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-half.out'
 tmux new-session -d -s heuristic-three-objs-one-default-indexing \
-'~/Maude/maude ~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-one.maude > \
-~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-one.maude > \
+~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-one.out'
 tmux new-session -d -s heuristic-three-objs-nml-half-default-indexing \
-'~/Maude/maude ~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-nml-half.maude > \
-~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-nml-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-nml-half.maude > \
+~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-nml-half.out'
 tmux new-session -d -s heuristic-three-objs-nml-one-default-indexing \
-'~/Maude/maude ~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-nml-one.maude > \
-~/heuristic-narrowing/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-nml-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-nml-one.maude > \
+~/heuristic-narrowing-search/test-default-indexing/heuristic-narrowing/three-objs/onethirdrule-nml-one.out'
--- run-test.sh
+++ run-test.sh
@@ -2,54 +2,54 @@
 
 # heuristic-narrow
 tmux new-session -d -s heuristic-arbitrary-half \
-'~/Maude/maude ~/heuristic-narrowing/test/heuristic-narrowing/arbitrary/onethirdrule-half.maude > \
-~/heuristic-narrowing/test/heuristic-narrowing/arbitrary/onethirdrule-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/heuristic-narrowing/arbitrary/onethirdrule-half.maude > \
+~/heuristic-narrowing-search/test/heuristic-narrowing/arbitrary/onethirdrule-half.out'
 tmux new-session -d -s heuristic-arbitrary-one \
-'~/Maude/maude ~/heuristic-narrowing/test/heuristic-narrowing/arbitrary/onethirdrule-one.maude > \
-~/heuristic-narrowing/test/heuristic-narrowing/arbitrary/onethirdrule-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/heuristic-narrowing/arbitrary/onethirdrule-one.maude > \
+~/heuristic-narrowing-search/test/heuristic-narrowing/arbitrary/onethirdrule-one.out'
 tmux new-session -d -s heuristic-arbitrary-nml-half \
-'~/Maude/maude ~/heuristic-narrowing/test/heuristic-narrowing/arbitrary/onethirdrule-nml-half.maude > \
-~/heuristic-narrowing/test/heuristic-narrowing/arbitrary/onethirdrule-nml-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/heuristic-narrowing/arbitrary/onethirdrule-nml-half.maude > \
+~/heuristic-narrowing-search/test/heuristic-narrowing/arbitrary/onethirdrule-nml-half.out'
 tmux new-session -d -s heuristic-arbitrary-nml-one \
-'~/Maude/maude ~/heuristic-narrowing/test/heuristic-narrowing/arbitrary/onethirdrule-nml-one.maude > \
-~/heuristic-narrowing/test/heuristic-narrowing/arbitrary/onethirdrule-nml-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/heuristic-narrowing/arbitrary/onethirdrule-nml-one.maude > \
+~/heuristic-narrowing-search/test/heuristic-narrowing/arbitrary/onethirdrule-nml-one.out'
 
 tmux new-session -d -s heuristic-three-objs-half \
-'~/Maude/maude ~/heuristic-narrowing/test/heuristic-narrowing/three-objs/onethirdrule-half.maude > \
-~/heuristic-narrowing/test/heuristic-narrowing/three-objs/onethirdrule-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/heuristic-narrowing/three-objs/onethirdrule-half.maude > \
+~/heuristic-narrowing-search/test/heuristic-narrowing/three-objs/onethirdrule-half.out'
 tmux new-session -d -s heuristic-three-objs-one \
-'~/Maude/maude ~/heuristic-narrowing/test/heuristic-narrowing/three-objs/onethirdrule-one.maude > \
-~/heuristic-narrowing/test/heuristic-narrowing/three-objs/onethirdrule-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/heuristic-narrowing/three-objs/onethirdrule-one.maude > \
+~/heuristic-narrowing-search/test/heuristic-narrowing/three-objs/onethirdrule-one.out'
 tmux new-session -d -s heuristic-three-objs-nml-half \
-'~/Maude/maude ~/heuristic-narrowing/test/heuristic-narrowing/three-objs/onethirdrule-nml-half.maude > \
-~/heuristic-narrowing/test/heuristic-narrowing/three-objs/onethirdrule-nml-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/heuristic-narrowing/three-objs/onethirdrule-nml-half.maude > \
+~/heuristic-narrowing-search/test/heuristic-narrowing/three-objs/onethirdrule-nml-half.out'
 tmux new-session -d -s heuristic-three-objs-nml-one \
-'~/Maude/maude ~/heuristic-narrowing/test/heuristic-narrowing/three-objs/onethirdrule-nml-one.maude > \
-~/heuristic-narrowing/test/heuristic-narrowing/three-objs/onethirdrule-nml-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/heuristic-narrowing/three-objs/onethirdrule-nml-one.maude > \
+~/heuristic-narrowing-search/test/heuristic-narrowing/three-objs/onethirdrule-nml-one.out'
 
 # vu-narrow
 tmux new-session -d -s vu-arbitrary-half \
-'~/Maude/maude ~/heuristic-narrowing/test/vu-narrow/arbitrary/onethirdrule-half.maude > \
-~/heuristic-narrowing/test/vu-narrow/arbitrary/onethirdrule-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/vu-narrow/arbitrary/onethirdrule-half.maude > \
+~/heuristic-narrowing-search/test/vu-narrow/arbitrary/onethirdrule-half.out'
 tmux new-session -d -s vu-arbitrary-one \
-'~/Maude/maude ~/heuristic-narrowing/test/vu-narrow/arbitrary/onethirdrule-one.maude > \
-~/heuristic-narrowing/test/vu-narrow/arbitrary/onethirdrule-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/vu-narrow/arbitrary/onethirdrule-one.maude > \
+~/heuristic-narrowing-search/test/vu-narrow/arbitrary/onethirdrule-one.out'
 tmux new-session -d -s vu-arbitrary-nml-half \
-'~/Maude/maude ~/heuristic-narrowing/test/vu-narrow/arbitrary/onethirdrule-nml-half.maude > \
-~/heuristic-narrowing/test/vu-narrow/arbitrary/onethirdrule-nml-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/vu-narrow/arbitrary/onethirdrule-nml-half.maude > \
+~/heuristic-narrowing-search/test/vu-narrow/arbitrary/onethirdrule-nml-half.out'
 tmux new-session -d -s vu-arbitrary-nml-one \
-'~/Maude/maude ~/heuristic-narrowing/test/vu-narrow/arbitrary/onethirdrule-nml-one.maude > \
-~/heuristic-narrowing/test/vu-narrow/arbitrary/onethirdrule-nml-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/vu-narrow/arbitrary/onethirdrule-nml-one.maude > \
+~/heuristic-narrowing-search/test/vu-narrow/arbitrary/onethirdrule-nml-one.out'
 
 tmux new-session -d -s vu-three-objs-half \
-'~/Maude/maude ~/heuristic-narrowing/test/vu-narrow/three-objs/onethirdrule-half.maude > \
-~/heuristic-narrowing/test/vu-narrow/three-objs/onethirdrule-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/vu-narrow/three-objs/onethirdrule-half.maude > \
+~/heuristic-narrowing-search/test/vu-narrow/three-objs/onethirdrule-half.out'
 tmux new-session -d -s vu-three-objs-one \
-'~/Maude/maude ~/heuristic-narrowing/test/vu-narrow/three-objs/onethirdrule-one.maude > \
-~/heuristic-narrowing/test/vu-narrow/three-objs/onethirdrule-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/vu-narrow/three-objs/onethirdrule-one.maude > \
+~/heuristic-narrowing-search/test/vu-narrow/three-objs/onethirdrule-one.out'
 tmux new-session -d -s vu-three-objs-nml-half \
-'~/Maude/maude ~/heuristic-narrowing/test/vu-narrow/three-objs/onethirdrule-nml-half.maude > \
-~/heuristic-narrowing/test/vu-narrow/three-objs/onethirdrule-nml-half.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/vu-narrow/three-objs/onethirdrule-nml-half.maude > \
+~/heuristic-narrowing-search/test/vu-narrow/three-objs/onethirdrule-nml-half.out'
 tmux new-session -d -s vu-three-objs-nml-one \
-'~/Maude/maude ~/heuristic-narrowing/test/vu-narrow/three-objs/onethirdrule-nml-one.maude > \
-~/heuristic-narrowing/test/vu-narrow/three-objs/onethirdrule-nml-one.out'
+'~/Maude/maude ~/heuristic-narrowing-search/test/vu-narrow/three-objs/onethirdrule-nml-one.maude > \
+~/heuristic-narrowing-search/test/vu-narrow/three-objs/onethirdrule-nml-one.out'
