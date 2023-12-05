cfb125bd9c2b132c928109d02bff817c6c77e46b
Byeongjee Kang <byeongjee.kang@gmail.com>
2023-12-04 23:07:36 -0500

    Add tests for different scoring functions

---
+++ run-different-scoring.sh
@@ -0,0 +1,22 @@
+
+tmux new-session -d -s 1-8 \
+'~/Maude/maude ~/heuristic-narrowing-search/test/scoring-functions/1-8.maude > \
+~/heuristic-narrowing-search/test/scoring-functions/1-8.out'
+tmux new-session -d -s 1-7 \
+'~/Maude/maude ~/heuristic-narrowing-search/test/scoring-functions/1-7.maude > \
+~/heuristic-narrowing-search/test/scoring-functions/1-7.out'
+tmux new-session -d -s 1-6 \
+'~/Maude/maude ~/heuristic-narrowing-search/test/scoring-functions/1-6.maude > \
+~/heuristic-narrowing-search/test/scoring-functions/1-6.out'
+tmux new-session -d -s 1-5 \
+'~/Maude/maude ~/heuristic-narrowing-search/test/scoring-functions/1-5.maude > \
+~/heuristic-narrowing-search/test/scoring-functions/1-5.out'
+tmux new-session -d -s 1-4 \
+'~/Maude/maude ~/heuristic-narrowing-search/test/scoring-functions/1-4.maude > \
+~/heuristic-narrowing-search/test/scoring-functions/1-4.out'
+tmux new-session -d -s 1-3 \
+'~/Maude/maude ~/heuristic-narrowing-search/test/scoring-functions/1-3.maude > \
+~/heuristic-narrowing-search/test/scoring-functions/1-3.out'
+tmux new-session -d -s 1-2 \
+'~/Maude/maude ~/heuristic-narrowing-search/test/scoring-functions/1-2.maude > \
+~/heuristic-narrowing-search/test/scoring-functions/1-2.out'
